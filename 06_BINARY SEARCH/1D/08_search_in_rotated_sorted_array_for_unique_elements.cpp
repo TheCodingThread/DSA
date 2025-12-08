3ae15440c3bcda9b1610c984c05418906d08b23c
TheCodingThread <sebinmatheweapen@gmail.com>
2025-12-08 23:37:47 +0530

    Q. Searching an element in a rotated sorted array of unique elements(brute force and optimal solution)

---
+++ 06_BINARY SEARCH/1D/08_search_in_rotated_sorted_array_for_unique_elements.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//brute force - use linear search
+
+//optimal - binary search
+int srchInArr(int arr[], int n){
+    int target;
+    cout << "Enter the target element: ";
+    cin >> target;
+
+    int low = 0, high = n-1;
+
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid] == target) return mid;
+        
+        if(arr[low] <= arr[mid]){
+            if(arr[low] <= target && target <= arr[mid]) high = mid-1;
+            else low = mid+1;
+        }
+        else{
+            if(arr[mid] <= target && target <= arr[high]) low = mid+1;
+            else high = mid-1;
+        }
+    }
+    
+    return -1;
+}
+//time complexity: O(log(2)n)
+
+int main(){
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int result = srchInArr(arr, n);
+    cout << result << endl;
+    
+    return 0;
+}
