3c3f30e5f4c16a2391c851531319b32575c73fdd
TheCodingThread <sebinmatheweapen@gmail.com>
2025-12-09 22:48:44 +0530

    Q. Find the number of rotations occured in a rotated sorted array of unique elements.

---
+++ 06_BINARY SEARCH/1D/11_find_no_of_rotations_of_sorted_array.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//find the sorted half, find the minimum element in that half and then eliminate that half
+int findRot(int arr[], int n){
+    int ans = INT_MAX, loc = -1;
+    int low = 0, high = n-1;
+
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[low] <= arr[mid]){
+            ans = min(ans, arr[low]);
+            if(arr[low] == ans) loc = low;
+            low = mid+1;
+        }
+        else{
+            ans = min(ans, arr[mid]);
+            if(arr[mid] == ans) loc = mid;
+            high = mid-1;
+        }
+    }
+
+    return loc;
+}
+//time complexity: O(log(2)n)
+
+int main(){
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int result = findRot(arr, n);
+    cout << "No. of rotations: " << result << endl;
+
+    return 0;
+}
