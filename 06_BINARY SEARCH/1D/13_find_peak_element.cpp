3c5fc0762a80057af1d368db73d068f0553bc9d6
TheCodingThread <sebinmatheweapen@gmail.com>
2025-12-17 00:22:53 +0530

    improved the optimal code

--- 06_BINARY SEARCH/1D/13_find_peak_element.cpp
+++ 06_BINARY SEARCH/1D/13_find_peak_element.cpp
@@ -23,11 +23,13 @@ int peakEle(int arr[], int n){
     int l = 1, h = n-2;
 
     while(l<=h){
-        int m = (l+h)/2;
+        int m = l + (h-l)/2;
         if(arr[m]>arr[m-1] && arr[m]>arr[m+1]) return arr[m];
         else if(arr[m-1]>arr[m]) h = m-1;
         else l = m+1;
     }
+
+    return 0;
 }
 
 int main(){
