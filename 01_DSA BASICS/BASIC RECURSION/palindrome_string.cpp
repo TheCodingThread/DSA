b67f24297b6585e0b0a9721eae4f4ca1359a4ae4
TheCodingThread <sebinmatheweapen@gmail.com>
2025-12-20 00:12:13 +0530

    improved code

--- 01_DSA BASICS/BASIC RECURSION/palindrome_string.cpp
+++ 01_DSA BASICS/BASIC RECURSION/palindrome_string.cpp
@@ -1,10 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int pdStr(string str, int n, int i){
+int pdStr(string &str, int i){
+    for(char &c : str) c = tolower(c);
+    
+    str.erase(remove_if(str.begin(), str.end(), [](char c) {
+        return !isalnum(c);
+    }), str.end());
+
+    int n = str.length();
+
     if(i>=n/2) return true;
     if(str[i]!=str[n-i-1]) return false;
-    return pdStr(str, n, i+1);
+    return pdStr(str, i+1);
 }
 
 int main(){
@@ -12,8 +20,7 @@ int main(){
     int n, i = 0;
     cout << "Enter a string: ";
     getline(cin, str);
-    n = str.length();
-    if(pdStr(str, n, i) == 1) cout << "The String is a palindrome.";
+    if(pdStr(str, i) == 1) cout << "The String is a palindrome.";
     else cout << "The String is not a palindrome.";
     return 0;
 }
