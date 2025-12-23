b4cac2b9d1d2e4e7b32370ae9fc05d1e07fd32bd
TheCodingThread <sebinmatheweapen@gmail.com>
2025-12-23 17:46:31 +0530

    Q. To check if the 2 given strings are isomorphic or not

---
+++ 07_STRINGS/04_isomorphic_strings.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isoStr(string s, string t){
+    char ms[128] = {0};
+    char mt[128] = {0};
+    for(int i=0; i<s.size(); i++){
+        if(ms[s[i]] != mt[t[i]]) return false;
+        ms[s[i]] = i+1;
+        mt[t[i]] = i+1;
+    }
+    return true;
+}
+
+int main(){
+    string s, t;
+    cout << "Enter the string 1: ";
+    cin >> s;
+    cout << "Enter the string 2: ";
+    cin >> t;
+
+    bool result = isoStr(s, t);
+    cout << result << endl;
+
+    return 0;
+}
