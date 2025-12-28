17bfad0277a4e41aea3a13847f778be44e6ad7c3
TheCodingThread <sebinmatheweapen@gmail.com>
2025-12-28 17:56:40 +0530

    Q. Implement atoi

---
+++ 07_STRINGS/10_str_to_int.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int strToInt(string s)
+{
+    int i = 0, n = s.size();
+
+    while (i < n && s[i] == ' ')
+        i++;
+
+    int sign = 1;
+    if (i < n && (s[i] == '+' || s[i] == '-'))
+    {
+        if (s[i] == '-')
+            sign = -1;
+        i++;
+    }
+
+    long long ans = 0;
+    while (i < n && isdigit(s[i]))
+    {
+        ans = ans * 10 + (s[i] - '0');
+
+        if (sign == 1 && ans > INT_MAX)
+            return INT_MAX;
+        if (sign == -1 && -ans < INT_MIN)
+            return INT_MIN;
+
+        i++;
+    }
+
+    return sign * ans;
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+
+    int r = strToInt(s);
+    cout << r;
+
+    return 0;
+}
