924aa0aad7ce2259949d718905dcf9d31911ce60
TheCodingThread <sebinmatheweapen@gmail.com>
2026-01-01 20:38:09 +0530

    Finding the length of a linked list

---
+++ 08_LINKED LIST/1D/04_length_of_ll.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+Node* arrToLL(vector<int> &arr){
+    Node* head = new Node(arr[0], nullptr);
+    Node* mover = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int lenOfLL(Node* head){
+    int cnt = 0;
+    Node* temp = head;
+    while(temp){
+        cnt++;
+        temp = temp->next;
+    }
+    return cnt;
+}
+
+int main(){
+    vector<int> ll = {2, 4, 6, 8};
+    Node* head = arrToLL(ll);
+    cout << lenOfLL(head);
+    return 0;
+}
