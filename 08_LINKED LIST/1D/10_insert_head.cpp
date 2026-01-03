d03c5cb666ad03b060618f40c1ca3f9b3691a273
TheCodingThread <sebinmatheweapen@gmail.com>
2026-01-03 18:43:33 +0530

    inserting a new head(not replacing) in a linked list

---
+++ 08_LINKED LIST/1D/10_insert_head.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+Node* arrToLL(vector<int> &arr){
+    Node* head = new Node(arr[0], nullptr);
+    Node* mover = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* insertHead(Node* head, int el){
+    return new Node(el, head);
+}
+//time complexity: O(1)
+
+int main(){
+    vector<int> ll = {2, 4, 6, 8};
+    Node* head = arrToLL(ll);
+    int el;
+    cout << "Enter the value to insert: ";
+    cin >> el;
+    head = insertHead(head, el);
+    print(head);
+    return 0;
+}
