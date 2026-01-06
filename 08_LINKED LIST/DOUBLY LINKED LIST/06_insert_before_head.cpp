c99464d6321f7fc568ea718b51d031e847cb69ff
TheCodingThread <sebinmatheweapen@gmail.com>
2026-01-06 14:59:23 +0530

    inserting a new head(no replacing) in a doubly linked list

---
+++ 08_LINKED LIST/DOUBLY LINKED LIST/06_insert_before_head.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    public:
+    Node(int data1, Node* next1, Node* prev1){
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+
+Node* arrToDoubLL(vector<int> &arr){
+    if(arr.size() == 0) return nullptr;
+    Node* head = new Node(arr[0], nullptr, nullptr);
+    Node* prev = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* insertHead(Node* head, int newNodeVal){
+    if(head == nullptr) head = new Node(newNodeVal, nullptr, nullptr);
+    Node* temp = new Node(newNodeVal, head, nullptr);
+    head->prev = temp;
+    return temp;
+}
+
+int main(){
+    vector<int> ll = {2, 4, 6, 8};
+    Node* head = arrToDoubLL(ll);
+    int newNodeVal;
+    cout << "Enter the node value: ";
+    cin >> newNodeVal;
+    head = insertHead(head, newNodeVal);
+    print(head);
+    return 0;
+}
