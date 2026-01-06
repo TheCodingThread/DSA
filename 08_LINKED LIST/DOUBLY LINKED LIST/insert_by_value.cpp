c080713488449f3ef8373341b0f34fdbfcd6b6ae
TheCodingThread <sebinmatheweapen@gmail.com>
2026-01-06 15:28:49 +0530

    inserting node at before the node in a doubly linked list whose value is given

---
+++ 08_LINKED LIST/DOUBLY LINKED LIST/insert_by_value.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    public:
+    Node(int data1, Node* next1, Node* prev1){
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+
+Node* arrToDoubLL(vector<int> &arr){
+    if(arr.size() == 0) return nullptr;
+    Node* head = new Node(arr[0], nullptr, nullptr);
+    Node* prev = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* insertByVal(Node* head, int newNodeVal, int orgNodeVal){
+    if(head == nullptr) return head;
+    int cnt = 0;
+    Node* temp = head;
+    if(head->data == orgNodeVal){
+        Node* newNode = new Node(newNodeVal, temp, nullptr);
+        temp->prev = newNode;
+        return newNode;
+    }
+    while(temp != nullptr){
+        if(temp->data == orgNodeVal){
+            Node* newNode = new Node(newNodeVal, temp, temp->prev);
+            temp->prev->next = newNode;
+            temp->prev = newNode;
+            return head;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> ll = {2, 4, 6, 8};
+    Node* head = arrToDoubLL(ll);
+    int newNodeVal, orgNodeVal;
+    cout << "Enter the node value: ";
+    cin >> newNodeVal;
+    cout << "Enter original node value: ";
+    cin >> orgNodeVal;
+    head = insertByVal(head, newNodeVal, orgNodeVal);
+    print(head);
+    return 0;
+}
