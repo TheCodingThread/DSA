180d0b528ecb5c15197eb6452385475d3c59a1bf
TheCodingThread <sebinmatheweapen@gmail.com>
2026-01-26 14:43:12 +0530

    Started RECURSION: Recursive implementation of atoi

---
+++ 09_RECURSION/01_recursive_implementation_of_atoi.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Atoi(string s){
+    int i=0, n = s.size();
+    while(i<n && s[i] == ' ') i++;
+
+    int sign = 1;
+    if(i<n && (s[i] == '+' || s[i] == '-')){
+        if(s[i] == '-') sign = -1;
+        i++;
+    }
+
+    long long ans = 0;
+    while(i<n && isdigit(s[i])){
+        ans = ans*10 + (s[i] - '0');
+        if(sign == 1 && ans > INT_MAX) return INT_MAX;
+        if(sign == -1 && -ans < INT_MIN) return INT_MIN;
+        i++;
+    }
+    return sign*ans;
+}
+//time complexity: O(n)
+
+int main(){
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+    cout << Atoi(s);
+    return 0;
+}
