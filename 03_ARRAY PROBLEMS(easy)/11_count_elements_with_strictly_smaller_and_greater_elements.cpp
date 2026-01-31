8ee0de045c8965943dd46721185b3e0923db758a
TheCodingThread <sebinmatheweapen@gmail.com>
2026-01-31 19:46:40 +0530

    Q. Counting the number of elements with strictly smaller and strictly greater elements in the given array

---
+++ 03_ARRAY PROBLEMS(easy)/11_count_elements_with_strictly_smaller_and_greater_elements.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countEle(vector<int> v){
+    int minNum = *min_element(v.begin(), v.end());
+        int maxNum = *max_element(v.begin(), v.end());
+        if(minNum == maxNum) return 0;
+        int minCount = count(v.begin(), v.end(), minNum);
+        int maxCount = count(v.begin(), v.end(), maxNum);
+        return v.size() - minCount - maxCount;
+}
+
+int main(){
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+    vector<int> v;
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    cout << countEle(v);
+    return 0;
+}
