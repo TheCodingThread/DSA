5564e06bed780cf44db3b8bf54fea600df996a02
TheCodingThread <sebinmatheweapen@gmail.com>
2026-03-11 21:44:42 +0530

    Included the optimal approach

--- 01_DSA BASICS/BASIC MATHS/print_all_divisors.cpp
+++ 01_DSA BASICS/BASIC MATHS/print_all_divisors.cpp
@@ -6,10 +6,21 @@ int main(){
     cout << "Enter the number: ";
     cin >> num;
     cout << "The divisors of " << num << " are: ";
-    for(int i = 1; i<=num; i++){
+    // for(int i = 1; i<=num; i++){
+    //     if(num%i == 0){
+    //         cout << i << " ";
+    //     }
+    // }
+    
+    //optimal - by observation
+    for(int i=0; i*i<=num; i++){
         if(num%i == 0){
             cout << i << " ";
+            if(i != num/i){
+                cout << num/i << " ";
+            }
         }
     }
+    //time complexity: O(sqrt(n))
     return 0;
 }
