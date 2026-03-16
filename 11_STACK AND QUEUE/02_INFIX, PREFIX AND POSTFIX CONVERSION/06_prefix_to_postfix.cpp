9c4a3ae87140320e3c3a182433e419d856f78a18
TheCodingThread <sebinmatheweapen@gmail.com>
2026-03-16 23:52:46 +0530

    prefix to postfix conversion

---
+++ 11_STACK AND QUEUE/02_INFIX, PREFIX AND POSTFIX CONVERSION/06_prefix_to_postfix.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string preToPost(string s){
+    int i=s.size()-1;
+    stack<string> st;
+    while(i>=0){
+        char c = s[i];
+        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+            string t;
+            t.push_back(c);
+            st.push(t);
+        }
+        else{
+            string op1 = st.top();
+            st.pop();
+            string op2 = st.top();
+            st.pop();
+            string con = op1 + op2 + s[i];
+            st.push(con);
+        }
+        i--;
+    }
+    return st.top();
+}
+//time complexity: O(2N); space complexity: O(N)
+
+int main(){
+    string s;
+    cout << "Enter string: ";
+    cin >> s;
+    cout << preToPost(s);
+    return 0;
+}
