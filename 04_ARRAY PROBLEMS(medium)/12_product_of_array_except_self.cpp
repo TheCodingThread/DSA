0f0c1c96bd29f3fb77fbc190c44c3bca16010d6b
TheCodingThread <sebinmatheweapen@gmail.com>
2026-03-19 16:48:39 +0530

    Q. Product of array except self

---
+++ 04_ARRAY PROBLEMS(medium)/12_product_of_array_except_self.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> answer(n, 1);
+
+    for(int i = 1; i < n; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+
+    int suffix = 1;
+    for(int i = n - 2; i >= 0; i--) {
+        suffix *= nums[i + 1];
+        answer[i] *= suffix;
+    }
+
+    return answer;
+}
+//time complexity: O(N)
