e84d1a05b59208e7b9fb7659c8ad04a7f0f83e89
TheCodingThread <sebinmatheweapen@gmail.com>
2026-04-16 18:11:41 +0530

    Implement function to check if an array represents a min-heap

---
+++ 13_HEAPS/INTRO/02_check_if_arr_represents_min_heap.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isArrMinHeap(vector<int>& arr, int n){
+    for(int i=0; i<(n-2)/2; i++){ //Start from the root and go up to the last internal node
+        if(arr[i] > arr[2*i+1]) return false;
+        if((2*i+2)<n && arr[i]>arr[2*i+2]) return false; //Check if the right child exists and violates the property
+        return true;
+    }
+}
+//time complexity: O(n)
+
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    vector<int> arr;
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    cout << isArrMinHeap(arr, n);
+    return 0;
+}
