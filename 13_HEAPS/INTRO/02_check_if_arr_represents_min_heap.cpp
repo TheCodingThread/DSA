ff10a451ebcb95f1bf2611367009addac19a2a05
TheCodingThread <sebinmatheweapen@gmail.com>
2026-04-16 18:13:27 +0530

    Fix loop condition in isArrMinHeap function to include last internal node

--- 13_HEAPS/INTRO/02_check_if_arr_represents_min_heap.cpp
+++ 13_HEAPS/INTRO/02_check_if_arr_represents_min_heap.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool isArrMinHeap(vector<int>& arr, int n){
-    for(int i=0; i<(n-2)/2; i++){ //Start from the root and go up to the last internal node
+    for(int i=0; i<=(n-2)/2; i++){ //Start from the root and go up to the last internal node
         if(arr[i] > arr[2*i+1]) return false;
         if((2*i+2)<n && arr[i]>arr[2*i+2]) return false; //Check if the right child exists and violates the property
         return true;
